1458b89480a33d1e13e3dd87e586bdedbf45de36
Antonio5672 <77584317+Antonio5672@users.noreply.github.com>
2021-02-11 22:47:38 +0100

    uploaded files

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    mousewheel: false,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+})
